ddd4e30e12ba11b8c299199428963dbbad4b2800
camiel34 <126400835+camiel34@users.noreply.github.com>
2024-06-10 20:21:37 +0100

    Changes back to original plan.

--- cocktails.js
+++ cocktails.js
@@ -3,38 +3,40 @@ This is the JavaScript for my page about how to make different cocktails
 */
 
 //Varaibles
-const cocktail listUrl = www.thecocktaildb.com/api/json/v1/1/list.php?c=list;
-const cocktailList= document.getElementById(*cocktail-list*);
+const ListUrl; www.thecocktaildb.com/api/json/v1/1/random.php;
+const cocktailList= document.getElementById (elementId) *cocktail-list;
 //when the page loads
-window.addEventListener(*load* , updateCocktailList);
+window.addEventListener *load*(updateCocktailList);
 
 //Retrieve the list of all cocktails from API
 function getCocktailList(){
   return  fetch(cocktailListUrl);
 }
 //Add cocktails to drop down list
-complexity is 3 everything is cool!
+{
+'(*Everything is cool*)';
+
 async function getCocktailList(){
-    return fetch(breedListUrl).then(response => response.json());
+    return fetch(cocktailListUrl).then(response => response.json());
 
 }
 function updateCocktailList(){
-    getCocktailList{}.then{function(data){}
+    getCocktailList .thenfunction
         //get cocktail name
         for(element in data.message){
 
-        }
+        
 
         //append to the select list
-        let option = createOption(element);
+        let optionption = createOption(element);
         cocktailList.appendChild(option);
     }
 
-  };
+  }
 
-} 
-function createOption(text){
-    let option = document.createElement(*option*);
+}
+function createOption (text){
+    let option = document. createElement *option*;
     option.textContent = text;
     return option;
 
